156c11ecfc0fa6043e598bc0882f94c376ad7f53
Brittany Arnold <barnold@csumb.edu>
2017-10-02 23:03:22 -0700

    Added index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+		<title> CST336 Fall 2017 </title>
+        <style>
+            body {
+                background-image: url("https://images7.alphacoders.com/404/404212.jpg");
+            }
+            h1 {
+                color: grey;
+                font-family: 'Press Start 2P', cursive;
+                font-size: 2.5em;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <div id="labs">
+            <h1 id="label-1">LABS:</h1>
+            <a href="/labs/lab1/index.php" target="_blank">Lab 1</a>
+            <a href="/labs/lab2/index.php" target="_blank">Lab 2</a>
+            <a href="/labs/Lab3/index.php" target="_blank" >Lab 3</a>
+            <a href="/labs/lab4/index.php" target="_blank" >Lab 4</a>
+        </div>
+        <div id="hw">
+            <h1 id="label-2">HOMEWORK:</h1>
+            <a href="/homework/homework1/index.php" target="_blank">HW 1</a>
+            <a href="/homework/homework2/index.php" target="_blank">HW 2</a>
+        </div>
+    </body>
+</html>
