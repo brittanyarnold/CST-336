ae7007f7958e9a2818b82c1a93b9b9340c0d603b
Brittany Arnold <barnold@csumb.edu>
2017-10-02 23:18:23 -0700

    Add files via upload

---
+++ homework/homework1/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title> Brittany Arnold's Programming Website </title>
+		<link href="css/styles.css" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+	</head>
+
+	<body>
+		<header>
+			<h1> PROGRAMMING LANGUAGES </h1>
+		</header>
+
+		<nav>
+			<hr id="line">
+			<a href="index.php">Home</a>
+			<a href="java.html">Java</a>
+			<a href="python.html">Python</a>
+			<a href="learn.html">Learn</a>
+		</nav>
+		<br /><br />
+
+		<div id="content">
+			<p> Welcome to this wonderful site about programming languages. </p>
+			<p> Programming languages are used by programmers to give sets of instructions for the computer to perform.
+			Languages are broken into two general components: syntax (structure) and semantics (logic or meaning).
+			There are literally hundreds of languages so it would be impossible to learn them all, but it's fun and 
+			useful to learn a few and become proficient in them. </p>
+			<img src="img/code.jpg" alt="Code pic" />
+		</div>
+
+		<footer>
+			<hr>
+			CST 336 Internet Programming. 2017&copy; Arnold<br />
+		</footer>
+	</body>
+</html>
+
