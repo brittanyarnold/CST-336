cc20f9628aa1af6a70d18ffa09b3d9fda6cdb732
Brittany Arnold <barnold@csumb.edu>
2017-10-02 23:20:54 -0700

    Add files via upload

---
+++ labs/lab1/index.php
@@ -0,0 +1,76 @@
+<html>
+
+	<head>
+		<meta charset="utf-8" />
+
+		<title> Brittany Arnold: Personal Website </title>
+
+		<link href="css/styles.css" rel="stylesheet" type="text/css">
+
+		<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+	</head>
+
+	
+	<body>
+
+		<header>
+
+			<h1> Brittany Arnold </h1>
+
+		</header>
+
+		<nav>
+
+			<hr width="50%" />
+
+			<a href="index.php">Home</a>
+
+			<a href="about.html">About</a>
+
+			<a href="contact.html">Contact</a>
+
+		</nav>
+
+		<br /><br />
+
+		<div id="content">
+
+			<figure id="me">
+
+				<img src="img/brittany_arnold.jpg" alt="Picture of Brittany Arnold" />
+
+			</figure>
+
+			<div id="welcomeText">
+
+				Hello! <br />
+
+				<p> Thank you for visiting my professional portfolio website. </p>
+
+				<p> I am a student at CSUMB pursing a B.S. in Software Engineering. </p>
+
+				<p> Feel free to contact me! </p>
+				
+				<em>If you think your users are idiots, only idiots will use it</em><br />
+				- Linus Torvalds
+			</div>
+
+		</div>
+
+		<footer>
+
+			<hr>
+
+			CST 336 Internet Programming. 2017&copy; Arnold<br />
+
+			<figure>
+
+				<img src="img/csumb.jpg" alt="Picture of CSUMB logo" />
+
+			</figure>
+
+		</footer>
+
+	</body>
+
+</html>
